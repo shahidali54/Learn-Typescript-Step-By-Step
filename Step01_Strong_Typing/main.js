22aeef7cc8ac22646019a085b3437ef1c86a2115
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-05-26 14:08:14 +0500

    Strong Typing

---
+++ Step01_Strong_Typing/main.js
@@ -0,0 +1,12 @@
+//strongly typed syntax
+let a = "Pakistan";
+a = "Canada";
+let b = 9;
+let c = true;
+//type inference
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
+export {};
