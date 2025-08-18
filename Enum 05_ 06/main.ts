63571c10dfb7e8e47263b261a3aaa3cef8a0f189
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-08-18 18:35:50 +0500

    Update main.ts

--- Enum 05_ 06/main.ts
+++ Enum 05_ 06/main.ts
@@ -1,4 +1,3 @@
-
 // enum Color1 {Red = 1, Green, Blue};
 // var colorName: string = Color1[2];
 // console.log(colorName);
@@ -27,9 +26,3 @@ console.log(colorNamee);
 const enum Color6 {Red = 1, Green = 2, Blue = 4};//can assign values to all
 var colorIndex2 = Color6["Blue"];
 console.log(colorIndex2);
-
-
-
-
-
-
