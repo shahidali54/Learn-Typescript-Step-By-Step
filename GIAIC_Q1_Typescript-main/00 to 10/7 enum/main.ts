cc0e8c9b1b1bf05031907318116808dcebb3a711
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-08-19 10:46:20 +0500

    Update main.ts

--- GIAIC_Q1_Typescript-main/00 to 10/7 enum/main.ts
+++ GIAIC_Q1_Typescript-main/00 to 10/7 enum/main.ts
@@ -1,18 +1,13 @@
-
-
 enum sports {football , cricket , hockey}
 
 let sport : string  = sports[2]
 console.log(sport);
 
 
-
-
 enum sports2 {football , cricket , hockey}
 
 let sport2 : sports2 = sports2.cricket
 console.log(sport2);
-
 
 
 enum sports3 {football=3 , cricket=10 , hockey} // can assign value
