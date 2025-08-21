0eec13868cb1b560144a58c4de4dcb538bb36499
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-08-21 11:15:02 +0500

    Update main.ts

--- 00 to 10/7 enum/main.ts
+++ 00 to 10/7 enum/main.ts
@@ -4,14 +4,10 @@ let sport : string  = sports[2]
 console.log(sport);
 
 
-
-
 enum sports2 {football , cricket , hockey}
 
 let sport2 : sports2 = sports2.cricket
 console.log(sport2);
-
-
 
 enum sports3 {football=3 , cricket=10 , hockey} // can assign value
 
