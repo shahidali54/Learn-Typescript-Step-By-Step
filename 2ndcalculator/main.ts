8496c03f02ea568d1993f73a40b6bd2a1d95251b
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-08-21 11:15:19 +0500

    Update main.ts

--- 2ndcalculator/main.ts
+++ 2ndcalculator/main.ts
@@ -1,57 +1,57 @@
-// import inquirer from "inquirer"
+import inquirer from "inquirer"
 
-// let answers = await inquirer.prompt([
-//     {
-//         type: "number",
-//         name: "num1",
-//         message: "enter your first number"
-//     },
-//     {
-//         type: "number",
-//         name: "num2",
-//         message: "enter your second number"
-//     },
-//     {
-//         type: "list",
-//         name: "operator",
-//         message: "select oparator",
-//         choices:["+","-","*","/"]
+let answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "enter your first number"
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "enter your second number"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "select oparator",
+        choices: ["+", "-", "*", "/"]
 
-//     }
-// ])
+    }
+])
 
-// let{num1, num2, operator} = answers
-// if (num1 && num2 && operator) {
-//     let result: number = 0
-//     if (operator === "+") {
-//         result = num1+num2
-//     }
+let { num1, num2, operator } = answers
+if (num1 && num2 && operator) {
+    let result: number = 0
+    if (operator === "+") {
+        result = num1 + num2
+    }
 
-//     if (operator === "-") {
-//         result = num1-num2
-//     }
+    if (operator === "-") {
+        result = num1 - num2
+    }
 
-//     if (operator === "*") {
-//         result = num1*num2
-//     }
+    if (operator === "*") {
+        result = num1 * num2
+    }
 
-//     if (operator === "/") {
-//         result = num1/num2
-//     }
-//     console.log("your result is", result);
-    
-// }
-// console.log(answers);
+    if (operator === "/") {
+        result = num1 / num2
+    }
+    console.log("your result is", result);
+
+}
+console.log(answers);
 
 // import inquirer from "inquirer";
 
 // const answers: {
-//     sentence:string
+//     sentence: string
 // } = await inquirer.prompt([
 //     {
 //         name: "sentence",
 //         type: "input",
-//         message:"enter your sentence"
+//         message: "enter your sentence"
 //     }
 // ])
 // let words = answers.sentence.trim().split(" ")
@@ -66,7 +66,8 @@
 // let answers = await inquirer.prompt([{
 //     name: "age",
 //     type: "number",
-//     message: "Enter your Age:"}
+//     message: "Enter your Age:"
+// }
 // ]);
 
 // console.log("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old.");
@@ -78,7 +79,8 @@
 // let answers = await inquirer.prompt([{
 //     name: "age",
 //     type: "number",
-//     message: "Enter your Age:"}
+//     message: "Enter your Age:"
+// }
 // ]);
 
 // console.log(chalk.blue("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old."));
