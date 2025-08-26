a6f124cedb0be4a34ea64f81c6ecfc55f52a9789
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-08-26 11:32:59 +0500

    Update main.ts

--- GIAIC_Q1_Typescript-main/00 to 10/8 array/main.ts
+++ GIAIC_Q1_Typescript-main/00 to 10/8 array/main.ts
@@ -1,18 +1,11 @@
 // array declare ways
-
 let a : number[] = []
-
 let b : Array<number> = []
-
 let c = ["Shahid" , 15]
-
 c.push("Mazari)")
-
 console.log(c);
-
 
 // function sm(name:string) {
 //     console.log(name)
 // }
 // sm("Shahid")
-
