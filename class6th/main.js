3aa3ebdb902058a89f6606592fd0da75b745cb2f
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-09-07 14:06:47 +0500

    Update main.js

--- class6th/main.js
+++ class6th/main.js
@@ -1,14 +1,14 @@
 let num1: number = 2;
 let num2: number = 5;
-let sum : number = num1 + num2
+let sum: number = num1 + num2
 console.log(sum)
 let num1: number = 7;
 let num2: number = 2;
-let sum : number = num1 - num2
+let sum: number = num1 - num2
 console.log(sum)
 let num1: number = 2;
 let num2: number = 5;
-let sum : number = num1 * num2
+let sum: number = num1 * num2
 console.log(sum)
 let num1: number = 20;
 let num2: number = 5;
@@ -16,16 +16,16 @@ let sum: number = num1 / num2
 console.log(sum)
 let num1: number = "2";
 let num2: number = 5;
-let sum : number = num1 + num2
+let sum: number = num1 + num2
 console.log(sum)
 answer 25 and this is concantication
 let num1: number = 5;
 let num2: number = 2;
-let sum : number = num1 ** num2
+let sum: number = num1 ** num2
 console.log(sum)
 let num1: number = 16;
 let num2: number = 3;
-let sum : number = num1 % num2
+let sum: number = num1 % num2
 console.log(sum)
 answer 1 and this is modulus
 let a: number = 5
