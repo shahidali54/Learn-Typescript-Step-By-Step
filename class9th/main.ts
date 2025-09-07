f596662a0950e71cee542c06f0b0267cf83cd9b8
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-09-07 14:07:00 +0500

    Update main.ts

--- class9th/main.ts
+++ class9th/main.ts
@@ -1,11 +1,11 @@
 // let isRaining = true
 // if (isRaining) {
 //     console.log("wear a rain coat");
-    
+
 // }
 // else {
 //     console.log("sunglasess");
-     
+
 // }                  
 //                        answer( if )
 
@@ -14,11 +14,11 @@
 // let eidi = 10000
 // if (eidi == 10000) {
 //     console.log("huarrrrrrr");
-    
+
 // }
 // else {
 //     console.log("arrrreeee yarrrrrr");
-    
+
 // }                
 //                        answer( if )
 
@@ -27,14 +27,14 @@
 // let eidi = 99999
 // if (eidi == 10000) {
 //     console.log("huarrrrrrr");
-    
+
 // }
 // else {
 //     console.log("arrrreeee yarrrrrr");
-    
+
 // }                 
 //                             answer( else)
- 
+
 // -----------------------------------
 
 
@@ -42,15 +42,15 @@
 
 // if (weather == "rainy") {
 //     console.log("wear a rain caot");
-    
+
 // }
 // else if (weather == "cloudy") {
 //     console.log("wear a banniyan");
-    
+
 // }
 // else {
 //     console.log("wear a sunglasess");
-    
+
 // }                   
 //                              answer( else if )
 
@@ -63,15 +63,15 @@
 
 // if (weather == "rainy") {
 //     console.log("wear a rain caot");
-    
+
 // }
 // else if (weather == "cloudy") {
 //     console.log("wear a banniyan");
-    
+
 // }
 // else if (weather == "cloudy") {
 //     console.log("wear a chadi banniyan");
-    
+
 //     }
 // else {
 //     console.log("wear a sunglasess");
@@ -84,19 +84,19 @@
 
 // if (eidi >= 10000) {
 //     console.log("go to burns road");
-    
+
 // }
 // else if (eidi >= 7500 && eidi < 10000) {
 //     console.log("go to quetta hotel");
-    
+
 // }
 // else if (eidi >= 5500 && eidi < 7500) {
 //     console.log("go to chai wala ");
-    
+
 // }
 // else {
 //     console.log("so jao ");
-    
+
 // }                    
 //                            answer(go to quetta hotel)
 
@@ -107,19 +107,19 @@
 
 // if (day == "baraat") {
 //     console.log("wear a red dress");
-    
+
 // }
 // else if (day == "mehndi") {
 //     console.log('wear a yellow dress');
-    
+
 // }
 // else if (day == "valima") {
 //     console.log("wear a maxi ");
-    
+
 // }
 // else {
 //     console.log("bulaya ee nahi");
-    
+
 // }               
 //                              answer(wear a yellow dress)
 
@@ -128,20 +128,20 @@
 
 function app() {
     console.log(" hello Shahid");
-    
+
 }
 // involve/call 
 app()
 
 function subhan() {
-    let cooked: string= "biryani"
+    let cooked: string = "biryani"
     console.log(cooked);
-    
+
 }
 function shahid() {
     let cooked: string = "karahi"
     console.log(cooked);
-    
+
 }
 subhan()
 shahid()
