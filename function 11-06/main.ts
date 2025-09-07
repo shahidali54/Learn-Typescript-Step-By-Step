b5e9f55f3c36aadac10aae71c8548c6230b51e31
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-09-07 14:07:43 +0500

    Update main.ts

--- function 11-06/main.ts
+++ function 11-06/main.ts
@@ -1,21 +1,22 @@
 // FUNCTION
 
-function sm1(name:string , id:number) {
-    
-    
-        console.log(name,id);
-        
-    
+function sm1(name: string, id: number) {
+
+
+        console.log(name, id);
+
+
 }
-sm1("Shahid" , 306343)
+sm1("Shahid", 306343)
 
 
 
 // LOOP FUNCTION
 
 function sm() {
-    
-    for (let a = 0; a < 1; a++)
-        console.log("Shahid",370195);
+
+        for (let a = 0; a < 1; a++)
+                console.log("Shahid", 370195);
 }
 sm()
+
